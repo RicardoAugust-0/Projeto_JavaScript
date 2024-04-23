d40bd1aff1d087548b23e5254b9bc743640b29f0
RicardoAugust-0 <166884977+RicardoAugust-0@users.noreply.github.com>
2024-04-23 13:20:23 -0300

    lição quase feita

--- Exercícios_JavaScript_ETEC/ex17/ex17.js
+++ Exercícios_JavaScript_ETEC/ex17/ex17.js
@@ -1,2 +1,10 @@
-/* Implemente um programa de uma máquina de café queofereça três opções de bebidas: café preto, café com leite e cappuccino. O usuário deve escolher uma opção e o programa deve exibir a bebida escolhida e seu valor.
+/* Implemente um programa de uma máquina de café que ofereça três opções de bebidas: café preto, café com leite e cappuccino. O usuário deve escolher uma opção e o programa deve exibir a bebida escolhida e seu valor.
  */
+var opcao = prompt("Escolha uma opção de café! :" + "\n" + "1 - Café preto" + "\n" + "2 - Café com leite" + "\n" + "3 - Cappuccino")
+var resultado = 0
+
+switch (resultado) {
+    case "1":
+    case "café preto":
+        alert("Sua bebida é \"café preto\" ")
+}
