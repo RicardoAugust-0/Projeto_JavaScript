a1bd11983c69d66ae63608d6ca392da077ec1257
RicardoAugust-0 <166884977+RicardoAugust-0@users.noreply.github.com>
2024-04-24 21:23:30 -0300

    pequenas alterações

---
+++ Exercícios_JavaScript_ETEC/ex18/ex18.js
@@ -0,0 +1,6 @@
+//Escreva um programa que exiba os números de 1 a 100 na tela em ordem decrescente.
+var num = 100;
+while (num >= 1 ) {
+    document.writeln(`<p>${num}</p>`);
+    num--;
+}
---
+++ Exercícios_JavaScript_ETEC/ex19/ex19.js
@@ -0,0 +1,15 @@
+/* Escreva um programa que leia:
+    - a quantidade de números que deverá processar;
+    - os números que deverá processar, e calcule e exiba, para cada número a ser processado o seu fatorial.
+    Lembrete: o fatorial de um número N é dado pela fórmula: N! = 1 * 2 * 3 * 4 * 5 * ... * N
+*/
+
+let num = 5; // Substitua pelo número desejado
+let fatorial = 1;
+
+while (num > 1) {
+    fatorial *= num;
+    num--;
+}
+
+alert(`O fatorial de ${num} (usando um loop while) é ${fatorial}.`);
