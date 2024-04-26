dda49bc9278826658734d46414f9873a325db637
RicardoAugust-0 <166884977+RicardoAugust-0@users.noreply.github.com>
2024-04-26 16:54:25 -0300

    vetor

---
+++ Aulas_CursoEmVideo/Aula15/ambiente.js
@@ -0,0 +1,10 @@
+let num = [0,5,9]
+num[3] = 8
+// num.push(x) coloca o valor na última posição
+// num.length mostra quantos valores tem dentro do vetor
+//num.sort() organiza os elementos em ordem crescente
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`Meu vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
---
+++ Aulas_CursoEmVideo/Aula15/vetoronscreen.js
@@ -0,0 +1,7 @@
+let valores = [8, 64, 1512, 7, 3, 12, 8]
+
+console.log(valores)
+
+for (let pos = 0;pos < valores.length;pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
