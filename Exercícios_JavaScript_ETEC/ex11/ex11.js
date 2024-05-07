5df34310e6bdac98c0fac7c7b88189ae12970d9f
RicardoAugust-0 <166884977+RicardoAugust-0@users.noreply.github.com>
2024-05-06 22:23:43 -0300

    a

--- Exercícios_JavaScript_ETEC/ex11/ex11.js
+++ Exercícios_JavaScript_ETEC/ex11/ex11.js
@@ -1,9 +1,13 @@
 //"Pare a execução do programa quando o usuário requisitar" se aplica em Estruturas de Repetição.
 
-var valor = prompt("coloque um número positivo ou negativo");
+try {
+  var valor = prompt("coloque um número positivo ou negativo");
 
-if (valor < 0) {
-    confirm(`O valor é Negativo! Valor: ${valor}`)
-} else if (valor > 0) {
-    confirm(`O valor é Positivo! Valor: ${valor}`)
+  if (valor < 0) {
+    confirm(`O valor é Negativo! Valor: ${valor}`);
+  } else if (valor > 0) {
+    confirm(`O valor é Positivo! Valor: ${valor}`);
+  }
+} catch (error) {
+  console.log("tá com erro");
 }
