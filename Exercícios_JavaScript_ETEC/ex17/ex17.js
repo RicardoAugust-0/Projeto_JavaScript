d8477546cfd2abde6de1b73952c2a36d83b4c871
RicardoAugust-0 <166884977+RicardoAugust-0@users.noreply.github.com>
2024-06-18 16:07:13 -0300

    alterações atividades

--- Exercícios_JavaScript_ETEC/ex17/ex17.js
+++ Exercícios_JavaScript_ETEC/ex17/ex17.js
@@ -1,20 +1,32 @@
 /* Implemente um programa de uma máquina de café que ofereça três opções de bebidas: café preto, café com leite e cappuccino. O usuário deve escolher uma opção e o programa deve exibir a bebida escolhida e seu valor.
  */
-var opcao = prompt("Escolha uma opção de café! :" + "\n" + "1 - Café preto" + "\n" + "2 - Café com leite" + "\n" + "3 - Cappuccino");
-
-switch (opcao) {
-    case "1":
-    case "café preto":
-        alert("Sua bebida é \"Café preto\" no valor de R$5,00 a xícara.");
-        break
-    case "2":
-    case "café com leite":
-        alert("Sua bebida é \"Café com leite\" no valor de R$5,50 a xícara.");
-        break
-    case "3":
-    case "cappuccino":
-        alert("Sua bebida é \"Capuccinno\" no valor de R$6,50 a xícara.");
-        break
-    default:
-        alert("Nenhuma opção válida!");
+function selecionarBebida(bebida) {
+    let nomeBebida;
+    let preco;
+  
+    switch (bebida) {
+      case 'cafe-preto':
+        nomeBebida = 'Café Preto';
+        preco = 2.5;
+        break;
+      case 'cafe-leite':
+        nomeBebida = 'Café com Leite';
+        preco = 3.0;
+        break;
+      case 'cappuccino':
+        nomeBebida = 'Cappuccino';
+        preco = 3.5;
+        break;
+      default:
+        nomeBebida = 'Bebida Desconhecida';
+        preco = 0;
+        break;
     }
+    exibirResultado(nomeBebida, preco);
+  }
+  
+  function exibirResultado(nomeBebida, preco) {
+    const resultadoDiv = document.getElementById('resultado');
+    resultadoDiv.innerHTML = `<p>Você escolheu: ${nomeBebida}</p>
+                              <p>Preço: R$ ${preco.toFixed(2)}</p>`;
+  }
